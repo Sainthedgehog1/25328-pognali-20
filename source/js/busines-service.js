12bf016579c068d3f5d1e9535bb1cee2d849c614
Sainthedgehog1 <svyatoiej@gmail.com>
2020-09-10 11:28:37 +0300

    test

--- source/js/busines-service.js
+++ source/js/busines-service.js
@@ -2,29 +2,31 @@
 
 /* Открытие модального окна */
 var popup = document.querySelector(".busines-service");
-var openButton = document.querySelector(".regular-service__link");
-var closeButton = document.querySelector(".busines-service__close-button");
+if(popup){
+  var openButton = document.querySelector(".regular-service__link");
+  var closeButton = document.querySelector(".busines-service__close-button");
 
-if (openButton) {
-  openButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  if (openButton) {
+    openButton.addEventListener("click", function (evt) {
+      evt.preventDefault();
 
-    popup.classList.add("busines-service--active");
-  })
-}
-
-if (closeButton) {
-  closeButton.addEventListener("click", function () {
-    popup.classList.remove("busines-service--active");
-  })
-}
-
-/* Закрытие модального окна по нажатию на Esc */
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("busines-service--active")) {
-      popup.classList.remove("busines-service--active");
-    }
+      popup.classList.add("busines-service--active");
+    })
   }
-});
+
+  if (closeButton) {
+    closeButton.addEventListener("click", function () {
+      popup.classList.remove("busines-service--active");
+    })
+  }
+
+  /* Закрытие модального окна по нажатию на Esc */
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("busines-service--active")) {
+        popup.classList.remove("busines-service--active");
+      }
+    }
+  });
+}
--- source/js/filter-toggle.js
+++ source/js/filter-toggle.js
@@ -2,18 +2,20 @@
 
 var toggles = document.querySelectorAll(".filter__toggle");
 
-var getCategory = function (toggle) {
-  return toggle.parentNode.querySelector(".filter__category");
-}
-
-toggles.forEach(function (toggle) {
-  toggle.classList.remove("filter__toggle--active");
-  getCategory(toggle).classList.remove("filter__category--active");
-
-  if (toggle) {
-    toggle.addEventListener("click", function (evt) {
-      toggle.classList.toggle("filter__toggle--active");
-      getCategory(toggle).classList.toggle("filter__category--active");
-    })
+if (toggles){
+  var getCategory = function (toggle) {
+    return toggle.parentNode.querySelector(".filter__category");
   }
-})
+
+  toggles.forEach(function (toggle) {
+    toggle.classList.remove("filter__toggle--active");
+    getCategory(toggle).classList.remove("filter__category--active");
+
+    if (toggle) {
+      toggle.addEventListener("click", function (evt) {
+        toggle.classList.toggle("filter__toggle--active");
+        getCategory(toggle).classList.toggle("filter__category--active");
+      })
+    }
+  })
+}
